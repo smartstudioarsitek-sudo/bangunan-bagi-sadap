1d175b728758a1cd21bf2458e6712dd5ae18ed93
smartstudioarsitek-sudo <smartstudioarsitek@gmail.com>
2026-01-12 17:04:44 +0700

    Update hidrolika.py

--- hidrolika.py
+++ hidrolika.py
@@ -52,3 +52,15 @@ def cek_neraca_air(Q_masuk, list_Q_keluar):
     seimbang = abs(selisih) < 0.001
     return seimbang, total_keluar, selisih
 
+def cek_neraca_air(Q_masuk, list_Q_keluar):
+    """
+    Memeriksa apakah debit masuk == total debit keluar.
+    Digunakan untuk simulasi Bangunan Bagi.
+    """
+    total_keluar = sum(list_Q_keluar)
+    selisih = Q_masuk - total_keluar
+    # Toleransi selisih 0.001 (untuk pembulatan)
+    seimbang = abs(selisih) < 0.001
+    return seimbang, total_keluar, selisih
+
+
